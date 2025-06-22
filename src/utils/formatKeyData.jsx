f31c08fef9dec6e2f656ff229c53d5c275df1c8e
Michael Bricout <bricout.michael@gmail.com>
2025-06-23 01:56:56 +0200

    Fixing link to KeyData icon

--- src/utils/formatKeyData.jsx
+++ src/utils/formatKeyData.jsx
@@ -4,28 +4,28 @@ export function formatKeyData(keyData) {
       id: "calories",
       label: "Calories",
       value: keyData.calorieCount.toLocaleString() + "kCal",
-      icon: "src/assets/calories-icon.png",
+      icon: "../src/assets/calories-icon.png",
       alt: "Calories icon"
     },
     {
       id: "proteins",
       label: "Protéines",
       value: keyData.proteinCount + "g",
-      icon: "src/assets/protein-icon.png",
+      icon: "../src/assets/protein-icon.png",
       alt: "Proteins icon"
     },
     {
       id: "carbs",
       label: "Glucides",
       value: keyData.carbohydrateCount + "g",
-      icon: "src/assets/carbs-icon.png",
+      icon: "../src/assets/carbs-icon.png",
       alt: "Carbs icon"
     },
     {
       id: "lipids",
       label: "Lipides",
       value: keyData.lipidCount + "g",
-      icon: "src/assets/fat-icon.png",
+      icon: "../src/assets/fat-icon.png",
       alt: "Lipids icon"
     }
   ];
